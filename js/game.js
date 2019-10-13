5c7006fa8658332f112fd75712cc4f3ca352fa6c
Anvar Nazarov <roman_emperor5@mail.ru>
2019-10-13 19:45:32 +0500

    JS file ready

---
+++ js/game.js
@@ -0,0 +1,14 @@
+let cvs = document.getElementById("canvas");
+let ctx = cvs.getContext("2d");
+
+let bird = new Image();
+let bg = new Image(); // Создание объекта
+let fg = new Image(); // Создание объекта
+let pipeUp = new Image(); // Создание объекта
+let pipeBottom = new Image(); // Создание объекта
+
+bird.src = "img/bird.png"; // Указание нужного изображения
+bg.src = "img/background.png"; // Аналогично
+fg.src = "img/foreground.png"; // Аналогично
+pipeUp.src = "img/pipeup.png"; // Аналогично
+pipeBottom.src = "img/pipebottom.png"; // Аналогично
